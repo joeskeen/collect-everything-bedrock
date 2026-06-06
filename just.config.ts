b95c76d6a8d47970625ab87b7e0eab6caf287735
Joe Skeen <joeskeen@outlook.com>
2026-06-06 12:42:29 -0600

    stamp add-on version to manifest version and display name

--- just.config.ts
+++ just.config.ts
@@ -15,17 +15,24 @@ import {
   updateWorldTask,
 } from "@minecraft/core-build-tasks";
 import path from "path";
-import { name as projectName, version as projectVersion } from './package.json';
-import 'varlock/auto-load';
+import fs from "fs";
+import { name as projectName, version as projectVersion, productName as displayName } from "./package.json";
+import semver from "semver";
+import "varlock/auto-load";
 
 process.env.PROJECT_NAME = projectName;
+const version = semver.parse(projectVersion);
+if (!version) {
+  throw new Error("invalid package version");
+}
+const versionArray = [version.major, version.minor, version.patch];
 
 const bundleTaskOptions: BundleTaskParameters = {
   entryPoint: path.join(__dirname, "./scripts/main.ts"),
   external: ["@minecraft/server", "@minecraft/server-ui"],
   outfile: path.resolve(__dirname, "./dist/scripts/main.js"),
-  minifyWhitespace: false,
-  sourcemap: true,
+  minifyWhitespace: true,
+  sourcemap: false,
   outputSourcemapPath: path.resolve(__dirname, "./dist/debug"),
 };
 const copyTaskOptions: CopyTaskParameters = {
@@ -41,15 +48,45 @@ task("lint", coreLint(["scripts/**/*.ts"], argv().fix));
 task("typescript", tscTask());
 task("bundle", bundleTask(bundleTaskOptions));
 task("build", series("typescript", "bundle"));
+task("stamp", () => {
+  (() => {
+    const rpManifestPath = path.resolve(__dirname, "resource_packs", projectName, "manifest.json");
+    const rpManifest = JSON.parse(fs.readFileSync(rpManifestPath).toString());
+    rpManifest.header.name = `${displayName} v${projectVersion} Resource Pack`;
+    rpManifest.header.version = versionArray;
+    // currently all modules and dependencies for the resource pack are internal, so we stamp all
+    rpManifest.modules.forEach((m: any) => (m.version = versionArray));
+    rpManifest.dependencies.forEach((m: any) => (m.version = versionArray));
+    console.log("stamped resource pack manifest", JSON.stringify(rpManifest));
+    fs.writeFileSync(rpManifestPath, JSON.stringify(rpManifest, null, 2));
+  })();
+
+  (() => {
+    const bpManifestPath = path.resolve(__dirname, "behavior_packs", projectName, "manifest.json");
+    const bpManifest = JSON.parse(fs.readFileSync(bpManifestPath).toString());
+    bpManifest.header.name = `${displayName} v${projectVersion}`;
+    bpManifest.header.version = versionArray;
+    // currently all modules for the behavior pack are internal, so we stamp all
+    // but we DON'T stamp dependencies since they are external
+    bpManifest.modules.forEach((m: any) => (m.version = versionArray));
+    console.log("stamped behavior pack manifest", JSON.stringify(bpManifest));
+    fs.writeFileSync(bpManifestPath, JSON.stringify(bpManifest, null, 2));
+  })();
+});
 task("clean-local", cleanTask(DEFAULT_CLEAN_DIRECTORIES));
 task("clean-collateral", cleanCollateralTask(STANDARD_CLEAN_PATHS));
 task("clean", parallel("clean-local", "clean-collateral"));
 task("copyArtifacts", copyTask(copyTaskOptions));
-task("package", series("clean-collateral", "copyArtifacts"));
+task("package", series("clean-collateral", "stamp", "copyArtifacts"));
 task(
   "local-deploy",
   watchTask(
-    ["scripts/**/*.ts", "behavior_packs/**/*.{json,lang,tga,ogg,png}", "resource_packs/**/*.{json,lang,tga,ogg,png}"],
+    [
+      "scripts/**/*.ts",
+      "behavior_packs/**/*.{json,lang,tga,ogg,png}",
+      "resource_packs/**/*.{json,lang,tga,ogg,png}",
+      "!**/manifest.json",
+    ],
     series("clean-local", "build", "package")
   )
 );
