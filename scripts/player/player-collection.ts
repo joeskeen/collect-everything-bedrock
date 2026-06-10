73e091e5e242989fbfb4b47c5dd571fe3acb3c44
Joe Skeen <joeskeen@outlook.com>
2026-06-09 20:38:32 -0600

    add ability to leash entity to collect it

--- scripts/player/player-collection.ts
+++ scripts/player/player-collection.ts
@@ -14,6 +14,7 @@ import { EffectCollector } from "../collections/effect/effect.collector";
 import { EnchantmentCollector } from "../collections/enchantment/enchantment.collector";
 import { EntityKilledCollector } from "../collections/entity/entity-killed.collector";
 import { EntityNamedCollector } from "../collections/entity/entity-named.collector";
+import { EntityLeashedCollector } from "../collections/entity/entity-leashed.collector";
 import { ItemCollector } from "../collections/item/item.collector";
 import { PlayerNotifier } from "./player-notifier";
 import { SOLID_STAR } from "../shared/emoji";
@@ -33,6 +34,7 @@ const COLLECTION_KEY = `${NAMESPACE}:collection`;
   { token: COLLECTORS_TOKEN, useClass: EnchantmentCollector },
   { token: COLLECTORS_TOKEN, useClass: EntityKilledCollector },
   { token: COLLECTORS_TOKEN, useClass: EntityNamedCollector },
+  { token: COLLECTORS_TOKEN, useClass: EntityLeashedCollector },
   { token: COLLECTORS_TOKEN, useClass: ItemCollector },
 ])
 @scoped(Lifecycle.ContainerScoped)
