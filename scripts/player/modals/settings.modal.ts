a8a51254dbdad97a3df8ee5c18dc654c8fb466aa
Joe Skeen <joeskeen@outlook.com>
2026-07-01 16:34:45 -0600

    hook up settings modal

---
+++ scripts/player/modals/settings.modal.ts
@@ -0,0 +1,59 @@
+import { inject, Lifecycle, scoped } from "tsyringe";
+import type { Player } from "@minecraft/server";
+import { PLAYER_TOKEN } from "../../shared/global-tokens";
+import { DifficultyLevel, PlayerSettingsService } from "../player-settings";
+import { DDUI, DDUI_TOKEN } from "../../ui/ui.tokens";
+
+export type SettingsFormResponses = [number];
+
+@scoped(Lifecycle.ContainerScoped)
+export class SettingsModal {
+  constructor(
+    @inject(PLAYER_TOKEN) private readonly player: Player,
+    @inject(PlayerSettingsService) private readonly playerSettings: PlayerSettingsService,
+    @inject(DDUI_TOKEN) private readonly ddui: DDUI
+  ) {}
+
+  async show(): Promise<void> {
+    const currentSettings = this.playerSettings.get();
+    const options: DifficultyLevel[] = ["basic", "committed", "insane"];
+
+    const difficulty = new this.ddui.ObservableNumber(options.indexOf(currentSettings.difficulty), {
+      clientWritable: true,
+    });
+
+    const form = new this.ddui.CustomForm(this.player, "Collect Everything! Settings").dropdown(
+      "Difficulty",
+      difficulty,
+      [
+        {
+          label: `Basic`,
+          value: 0,
+          description: `one of each thing by minecraft ID string (i.e. one horse)`,
+        },
+        {
+          label: `Committed`,
+          value: 1,
+          description: `collect each variant *type* (i.e. each horse color and each horse pattern)`,
+        },
+        {
+          label: `Insane`,
+          value: 2,
+          description: `collect each variant *combination* (i.e. each horse color/pattern combination)
+              (warning: this includes 2000+ tropical fish combinations!)`,
+        },
+      ],
+      { description: "Changes the collection goals." } as any
+    );
+
+    try {
+      await form.show();
+      const difficultyLevel = difficulty.getData();
+      this.playerSettings.change({
+        difficulty: options[difficultyLevel],
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
--- scripts/player/player-commands/settings.command.ts
+++ scripts/player/player-commands/settings.command.ts
@@ -1,66 +1,21 @@
 import { inject, Lifecycle, scoped } from "tsyringe";
 import { addOnCommand, CommandHandler, customCommandStatuses } from "../../system/add-on-command";
-import type { Player, CustomCommandResult, CustomCommandOrigin, System } from "@minecraft/server";
-import { PLAYER_TOKEN, SYSTEM_TOKEN } from "../../shared/global-tokens";
-import { DifficultyLevel, PlayerSettingsService } from "../player-settings";
-import { DDUI, DDUI_TOKEN } from "../../ui/ui.tokens";
+import type { CustomCommandResult, System } from "@minecraft/server";
+import { SYSTEM_TOKEN } from "../../shared/global-tokens";
+import { SettingsModal } from "../modals/settings.modal";
 
 export type SettingsFormResponses = [number];
 
 @scoped(Lifecycle.ContainerScoped)
 export class PlayerSettingsCommand implements CommandHandler {
   constructor(
-    @inject(PLAYER_TOKEN) private readonly player: Player,
     @inject(SYSTEM_TOKEN) private readonly system: System,
-    @inject(PlayerSettingsService) private readonly playerSettings: PlayerSettingsService,
-    @inject(DDUI_TOKEN) private readonly ddui: DDUI
+    @inject(SettingsModal) private readonly settingsModal: SettingsModal
   ) {}
 
-  handleCommand(event: CustomCommandOrigin, args: any[]): CustomCommandResult {
+  handleCommand(): CustomCommandResult {
     this.system.run(() => {
-      const currentSettings = this.playerSettings.get();
-      const options: DifficultyLevel[] = ["basic", "committed", "insane"];
-
-      const difficulty = new this.ddui.ObservableNumber(options.indexOf(currentSettings.difficulty), {
-        clientWritable: true,
-      });
-
-      new this.ddui.CustomForm(this.player, "Collect Everything! Settings")
-        .divider()
-        .dropdown(
-          "Difficulty",
-          difficulty,
-          [
-            {
-              label: `Basic`,
-              value: 0,
-              description: `one of each thing by minecraft ID string (i.e. one horse)`,
-            },
-            {
-              label: `Committed`,
-              value: 1,
-              description: `collect each variant *type* (i.e. each horse color and each horse pattern)`,
-            },
-            {
-              label: `Insane`,
-              value: 2,
-              description: `collect each variant *combination* (i.e. each horse color/pattern combination)
-              (warning: this includes 2000+ tropical fish combinations!)`,
-            },
-          ],
-          { description: "Changes the collection goals." } as any
-        )
-        .divider()
-        .show()
-        .then((x) => {
-          const difficultyLevel = difficulty.getData();
-          this.playerSettings.change({
-            difficulty: options[difficultyLevel],
-          });
-        })
-        .catch((e) => {
-          console.error(e);
-        });
+      this.settingsModal.show();
     });
 
     return { status: customCommandStatuses.Success };
